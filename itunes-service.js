e0d72c792fd6705b380d27b86bdbb7de39df83e4
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-05-03 15:15:38 -0600

    push before grading

--- itunes-service.js
+++ itunes-service.js
@@ -8,13 +8,19 @@ function ItunesService(){
       
       //changes the button to loading while songs load
       $('#get-music-button').text('LOADING....');
+
+      function swapUrlSize(url, pixels) {
+        var sizeString = `${pixels}x${pixels}`;
+        var newURL = url.replace("60x60", sizeString);
+        return newURL;
+      }
       
       //modifies the objects to reduce the excess data
       return $.getJSON(url).then(function(response){
         var songList = response.results.map(function (song) {
                   return {
                       title: song.trackName,
-                      albumArt: song.artworkUrl60,
+                      albumArt: swapUrlSize(song.artworkUrl60, 200),
                       artist: song.artistName,
                       collection: song.collectionName,
                       price: song.collectionPrice,
